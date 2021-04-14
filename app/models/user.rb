dd3684eebde1216747a09412cec6c40c022c52c2
juancarlos <juancarlosenrique103@gmail.com>
2021-04-14 02:56:39 -0500

    Added unique indes to user model :zap:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :name, :email, presence: true
   validates :name, length: { minimum: 1, maximum: 60 }
   validates :email, length: { minimum: 4, maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
 end
