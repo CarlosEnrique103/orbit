1ec196906dfca97da83488671d3509b3698d993c
juancarlos <juancarlosenrique103@gmail.com>
2021-04-14 17:59:04 -0500

    Added container with the errros messages :zap:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,29 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :set_user
-  def new; end
-
+  before_action :set_user, only: %i[show update destroy]
   def show; end
- 
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # code succesfull
+    else
+      render 'new'
+    end
+  end
+
   def set_user
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :passoword_confirmation)
   end
 end
