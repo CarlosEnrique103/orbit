ab8546d04b45b9d89ca340dd644a0c3ccc59dc8d
juancarlos <juancarlosenrique103@gmail.com>
2021-04-14 01:47:10 -0500

    Added indent for files and add test of presence to user model :zap:

--- app/controllers/html_pages_controller.rb
+++ app/controllers/html_pages_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HtmlPagesController < ApplicationController
   def home; end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   root 'html_pages#home'
   get 'about', to: 'html_pages#about'
